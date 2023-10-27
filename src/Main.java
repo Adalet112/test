4834ab89988e3fbbfd4761a0e49550a1323a500a
a.amalbay <a.amalbay@dudar.kz>
2023-10-27 15:17:10 +0600

    Reformade code

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,35 @@
 public class Main {
     public static void main(String[] args) {
 
-    int a = 10;
-    int b = 2;
+        int a = 10;
+        int b = 2;
 
-        System.out.println(sum(a,b));
-        System.out.println(sub(a,b));
-        System.out.println(mul(a,b));
-        System.out.println(div(a,b));
-        System.out.println(mod(a,b));
+        System.out.println(sum(a, b));
+        System.out.println(sub(a, b));
+        System.out.println(mul(a, b));
+        System.out.println(div(a, b));
+        System.out.println(mod(a, b));
 
     }
 
     public static int sum(int a, int b) {
-        return a+b;
+        return a + b;
     }
 
     public static int sub(int a, int b) {
-        return a-b;
+        return a - b;
     }
 
     public static int mul(int a, int b) {
-        return a*b;
+        return a * b;
     }
 
     public static int div(int a, int b) {
-        return a/b;
+        return a / b;
     }
 
     private static int mod(int a, int b) {
-        return  a%b;
+        return a % b;
     }
 
 }
