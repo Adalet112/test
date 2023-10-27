e2525369e83166a5ac16d1b89c28ffca5ddd4f06
a.amalbay <a.amalbay@dudar.kz>
2023-10-27 14:59:37 +0600

    Add sub func

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,16 @@ public class Main {
     int b = 2;
 
         System.out.println(sum(a,b));
+        System.out.println(sub(a,b));
 
     }
 
     public static int sum(int a, int b) {
         return a+b;
+    }
+
+    public static int sub(int a, int b) {
+        return a-b;
     }
 
 }
